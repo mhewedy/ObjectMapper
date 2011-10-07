03eba37949e46da8a4083ec33c0801030c829892
MuhammadHewedy <daziplqa@gmail.com>
2011-10-07 11:39:47 +0300

    Edited ObjectMapperTester/ObjectMapper.m via GitHub

--- ObjectMapperTester/ObjectMapper.m
+++ ObjectMapperTester/ObjectMapper.m
@@ -221,9 +221,11 @@ static const char *getPropertyType(objc_property_t property)
     char *state = buffer, *attribute;
     while ((attribute = strsep(&state, ",")) != NULL) 
     {
-        if (attribute[0] == 'T') 
+        if (attribute && strlen(attribute) > 3 && attribute[0] == 'T')
         {
-            return (const char *)[[NSData dataWithBytes:(attribute + 3) length:strlen(attribute) - 4] bytes];
+            NSUInteger i = strlen(attribute) - 4;
+            //NSLog(@"attribute = %s, strlen = %lu, i=%lu",attribute, strlen(attribute), i);
+            return (const char *)[[NSData dataWithBytes:(attribute + 3) length:i] bytes];
         }
     }
     return "@";
@@ -243,6 +245,5 @@ static const char *getPropertyType(objc_property_t property)
     [objectMapper release];
 }
 @end
-
 
 
